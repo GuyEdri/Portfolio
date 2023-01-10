887b4067af556f8291cd39341f34ab5053828786
Guy Edri <guyedrix2@gmail.com>
2023-01-10 17:33:23 +0200

    in App.js removing Unnecessary imports About css: fixing container dimensions Hero css: fixing the padding make it even with all other section Navbar Scrolling to top and not to hero

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -5,49 +5,51 @@ import classes from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <nav className={classes.mainClass}>
-      <ScrollLink
-        to="hero"
-        smooth={true}
-        duration={1000}
-        className={classes.button}
-      >
-        {" "}
-        &uarr;
-      </ScrollLink>
-      <ul className={classes.nav}>
-        <Link to="/" className={classes.navFirstEl}>
-          Guy Edri
-        </Link>
-        <li>
-          <ScrollLink
-            to="Skills"
-            smooth={true}
-            duration={1000}
-            offset={-50}
-            className={classes.Link}
-          >
-            Skills
-          </ScrollLink>
-        </li>
-        <li>
-          <Link to="/projects" className={classes.Link}>
-            Projects
+    <section id="nav">
+      <nav className={classes.mainClass}>
+        <ScrollLink
+          to="nav"
+          smooth={true}
+          duration={1000}
+          className={classes.button}
+        >
+          {" "}
+          &uarr;
+        </ScrollLink>
+        <ul className={classes.nav}>
+          <Link to="/" className={classes.navFirstEl}>
+            Guy Edri
           </Link>
-        </li>
-        <li className={classes.marginRight}>
-          <ScrollLink
-            className={classes.hire + " " + classes.Link}
-            to="hireMe"
-            smooth={true}
-            duration={1000}
-            offset={-60}
-          >
-            Hire Me &#8594;
-          </ScrollLink>
-        </li>
-      </ul>
-    </nav>
+          <li>
+            <ScrollLink
+              to="Skills"
+              smooth={true}
+              duration={1000}
+              offset={-50}
+              className={classes.Link}
+            >
+              Skills
+            </ScrollLink>
+          </li>
+          <li>
+            <Link to="/projects" className={classes.Link}>
+              Projects
+            </Link>
+          </li>
+          <li className={classes.marginRight}>
+            <ScrollLink
+              className={classes.hire + " " + classes.Link}
+              to="hireMe"
+              smooth={true}
+              duration={1000}
+              offset={-60}
+            >
+              Hire Me &#8594;
+            </ScrollLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
   );
 };
 
